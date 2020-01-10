67472a73d66d1d7c3639e4c49442069ef8a66013
Michal <michal.ciombor@gmail.com>
2020-01-10 20:16:43 +0100

    css added + js active class mechanis changed fo observer

--- app.js
+++ app.js
@@ -8,7 +8,30 @@ const menu = document.querySelector(".main-header>.container");
 const burger = document.querySelector(".burger");
 const burgerSpan = burger.querySelectorAll("span");
 let offsets = [];
-section = document.querySelectorAll("section");
+let section = document.querySelectorAll("section");
+let sectionWithId=[];
+for(sec of section){
+  if(sec.hasAttribute("id")){
+    sectionWithId.push(sec);
+  }
+}
+
+const options={
+  threshold:.54,
+}
+function checkHeight(entries){
+  entries.forEach(entry=>{
+    let visibleSection = entry.target.getAttribute("data-index");
+    for(menuItem of menuItems){
+      menuItem.classList.remove("active");
+      menuItems[visibleSection].classList.add("active");
+    }
+  });
+ 
+}
+
+let observer = new IntersectionObserver(checkHeight, options);
+sectionWithId.forEach(el=>{observer.observe(el)});
 
 // disabling active class adding  by scroll on mobiledevices
 
@@ -45,12 +68,22 @@ if (window.innerWidth > 800) {
   });
 }
 
-for (menuItem of menuItems) {
-  menuItem.addEventListener("click", function() {
-    menuItems.forEach(el => el.classList.remove("active"));
-    this.classList.add("active");
-  });
-}
+/*add active class to menu by click */
+// for (menuItem of menuItems) {
+//   if(menuItem.classList.contains("facebook-icon")||menuItem.classList.contains("twitter-icon")){
+//     menuItem.classList.remove("active");
+//   }else{
+//    menuItem.addEventListener("click", function() {
+//     menuItems.forEach(el => el.classList.remove("active"));
+//     this.classList.add("active");
+//   });
+// }
+// }
+
+
+
+ 
+
 
 // burger-nav-button
 burger.addEventListener("click", function() {
